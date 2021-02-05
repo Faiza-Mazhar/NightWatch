d64ee64e0af013c4e2d42a3b953e61c26ed848e4
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-02-05 14:56:08 +0000

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Faiza Mazhar on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
